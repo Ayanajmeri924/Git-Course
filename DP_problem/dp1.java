9dd2f67b0534ad7f97924097ad2c5c3c42d25811
Ayan <ayanajmeri924@gmail.com>
2026-01-08 21:41:00 +0530

    DP Problem

---
+++ DP_problem/dp1.java
@@ -0,0 +1,34 @@
+package DP_problem;
+
+public class dp1 {
+    public static void main(String[] args) {
+        String s1="ayana";
+        String s2="sohana";
+
+        int m=s1.length();
+        int n=s2.length();
+
+        int dp[][]=new int[m+1][n+1];
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    sb=sb.append(dp[i][j]);
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    sb=sb.append(dp[i][j]);
+
+                }
+            }
+
+        }
+
+
+        System.out.println(dp[m][n]);
+        System.out.println(sb);
+
+
+    }
+}
